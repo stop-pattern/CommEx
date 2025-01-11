045cd876fd77900a7ee07852238e4754f092402a
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-01-12 00:23:41 +0900

    change dir

--- CommEx/Serial/Common/ISerialControl.cs
+++ CommEx/Serial/Common/ISerialControl.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CommEx.Serial
 {
-    internal interface ISerialControl
+    public interface ISerialControl
     {
         /// <summary>
         /// ポートを開ける前に呼ばれる
@@ -57,4 +57,30 @@ namespace CommEx.Serial
         /// <param name="e"></param>
         void ScenarioClosed(EventArgs e);
     }
+
+    /// <summary>
+    /// 入力されたキーを送信する
+    /// </summary>
+    /// <param name="nInputs"></param>
+    /// <param name="pInputs"></param>
+    /// <param name="cbsize"></param>
+    //[DllImport("user32.dll", SetLastError = true)]
+    //public extern static void SendInput(int nInputs, Input[] pInputs, int cbsize);
+
+    /// <summary>
+    /// BveHacker と Native の初期化
+    /// </summary>
+    /// <param name="bveHacker"></param>
+    /// <param name="native"></param>
+    /// <exception cref="ArgumentNullException">引数がnullの時に投げる例外</exception>
+    //public static void Load(IBveHacker bveHacker, INative native)
+    //{
+    //    native = native ?? throw new ArgumentNullException(nameof(native));
+    //    bveHacker = bveHacker ?? throw new ArgumentNullException(nameof(bveHacker));
+    //}
+    //public static void Load(IBveHacker bveHacker)
+    //{
+    //    bveHacker = bveHacker ?? throw new ArgumentNullException(nameof(bveHacker));
+    //}
+
 }
